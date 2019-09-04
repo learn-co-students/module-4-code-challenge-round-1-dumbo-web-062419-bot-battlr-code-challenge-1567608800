6e9c297cc8d22070d550d80eebbecba5b08ccf9b
github username <github email address>
2019-09-04 12:34:36 -0400

    done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -3,13 +3,30 @@ import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
   //your code here
+  state = {
+	  team: []
+  }
+
+handleClick = (id) => {
+	console.log("hellooooooooo", id)
+	if(id === id){
+	this.setState({ team: [...this.state.team, id]})
+	}
+	else {
+		this.state.team
+	}
+}
 
   render(){
+	  
+	  const mappedBots = this.props.bots.map((botObj) => <BotCard key={botObj.id} bot={botObj} handleClick={this.handleClick}/>)
+	// console.log(this.props)
+
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
     		  Collection of all bots
+			  {mappedBots}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,11 +1,27 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+state = {
+  bots : []
+}
+
+componentDidMount() {
+  fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+  .then(res => res.json())
+  .then(
+    (botsData) => this.setState({bots: botsData})
+  )}
+
   render() {
+    // console.log(this.state.bots)
     return (
       <div>
+        <YourBotArmy />
+        <BotCollection bots={this.state.bots}/>
         {/* put your components here */}
       </div>
     );
