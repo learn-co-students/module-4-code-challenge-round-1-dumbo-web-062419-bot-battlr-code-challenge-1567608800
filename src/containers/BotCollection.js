9ef293ada28c1b7711309e558e043d7a4ff3cee9
Jahaziel Guzman <jahaziel.guzman93@gmail.com>
2019-09-04 11:32:32 -0400

    Bats can be added to my army and remove from it also

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -3,13 +3,20 @@ import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
   //your code here
+  constructor(props) {
+    super(props)
+  }
 
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {this.props.bots.map(bot =>
+            <BotCard
+              bot={bot}
+              key={bot.id}
+              addToArmy={this.props.addToArmy} 
+          />)}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,36 @@
 import React from "react";
+import BotCollection from "./BotCollection"
+import YourBotArmy from "./YourBotArmy"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    allBots: [],
+    yourBots: []
+  }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res => res.json())
+    .then(bots => this.setState({allBots: bots}))
+  }
+
+  addToArmy = (bot) => {
+    if (!this.state.yourBots.find(b => b.id === bot.id)) {
+      this.setState({yourBots: [...this.state.yourBots, bot]})
+    }
+  }
+
+  removeFromArmy = (bot) => {
+    this.setState({yourBots: this.state.yourBots.filter(b => b.id !== bot.id) })
+  }
 
   render() {
     return (
       <div>
         {/* put your components here */}
+        <YourBotArmy bots={this.state.yourBots} removeFromArmy={this.removeFromArmy}/>
+        <BotCollection bots={this.state.allBots} addToArmy={this.addToArmy}/>
       </div>
     );
   }
